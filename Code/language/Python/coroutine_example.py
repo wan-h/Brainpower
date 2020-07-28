a92434f902cc844a7777082a906870d7ab26bc0d
wanhui <13658247573@163.com>
2020-07-29 00:39:04 +0800

    add Code/language/Python/coroutine_example.py

---
+++ Code/language/Python/coroutine_example.py
@@ -0,0 +1,76 @@
+# coding: utf-8
+# Author: wanhui0729@gmail.com
+
+'''
+协程具有程极高的执行效率。
+子程序切换不是线程切换，而是由程序自身控制，因此，没有线程切换的开销，和多线程比，线程数量越多，协程的性能优势就越明显。
+不需要多线程的锁机制，因为只有一个线程，也不存在同时写变量冲突，在协程中控制共享资源不加锁，只需要判断状态就好了，所以执行效率比多线程高很多。
+
+Python对协程的支持是通过generator实现的。
+在generator中，我们不但可以通过for循环来迭代，还可以不断调用next()函数获取由yield语句返回的下一个值。
+但是Python的yield不但可以返回一个值，它还可以接收调用者发出的参数。
+'''
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    # 启动生成器
+    c.send(None)
+    n = 0
+    while n < 5:
+        n += 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return %s' % r)
+    c.close()
+
+def example_1():
+    c = consumer()
+    produce(c)
+
+# 实现并发
+'''
+Python 3.4版本引入的标准库asyncio, 内置了对异步IO的支持
+'''
+
+import threading
+import asyncio
+
+# 装饰器定义函数为协程类型
+@asyncio.coroutine
+def hello_1():
+    print('Hello world! (%s)' % threading.current_thread())
+    # asyncio.sleep(1)也是协程，所以主线程不会等待，去执行其他可以执行的coroutine了，因此可以实现并发执行。
+    yield from asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.current_thread())
+
+def example_2():
+    loop = asyncio.get_event_loop()
+    tasks = [hello_1(), hello_1()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
+# 为了简化并更好地标识异步IO，从Python 3.5开始引入了新的语法async和await，可以让coroutine的代码更简洁易读。
+async def hello_2():
+    print('Hello world! (%s)' % threading.current_thread())
+    await asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.current_thread())
+
+def example_3():
+    loop = asyncio.get_event_loop()
+    tasks = [hello_2(), hello_2()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
+
+if __name__ == '__main__':
+    # example_1()
+    # example_2()
+    example_3()
