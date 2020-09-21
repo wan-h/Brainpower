8a8e7881ef798938adc311d452be432a31c84fc0
wan-h <13658247573@163.com>
2020-09-21 13:59:27 +0800

    update Code/Tools/FastAPI/image

--- Code/Tools/FastAPI/image/image_client.py
+++ Code/Tools/FastAPI/image/image_client.py
@@ -1,23 +1,31 @@
 # coding: utf-8
 # Author: wanhui0729@gmail.com
 
+import time
 import os, sys
 import base64
 import json
+import cv2
+import numpy as np
+from io import BytesIO
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root_path)
-
 from image_server import app
 from fastapi.testclient import TestClient
 
 # 初始化测试 client
 c_client = TestClient(app)
 
-image_path = os.path.join(root_path, 'test.jpg')
+# image_path = os.path.join(root_path, 'test.jpg')
+# image_path = os.path.join(root_path, '512.png')
+# image_path = os.path.join(root_path, '1080P.png')
+image_path = os.path.join(root_path, '2K.png')
+# image_path = os.path.join(root_path, '4K.png')
 
-if __name__ == '__main__':
-    url = "/images/1"
+def test_base64_api():
+    url = "/images/base64/1"
+    print(url)
     with open(image_path, 'rb') as f:
         image_data = f.read()
         base64_data = base64.b64encode(image_data)
@@ -25,3 +33,37 @@ if __name__ == '__main__':
     resp = c_client.post(url=url, json={'images': [image_str]})
     res_data = resp.json()
     print(res_data)
+
+def test_numpy_api():
+    url = "/images/numpy/1"
+    print(url)
+    image_ndarray = cv2.imread(image_path)
+    shape = image_ndarray.shape
+    resp = c_client.post(url=url, params={'shape': json.dumps(shape)}, files={'file': image_ndarray.tobytes()})
+    res_data = resp.json()
+    print(res_data)
+
+"""
+！！！
+依赖安装
+pip install python-multipart
+"""
+def test_file_api():
+    url = "/images/file/1"
+    print(url)
+    resp = c_client.post(url=url, files={'file': open(image_path, 'rb')})
+    res_data = resp.json()
+    print(res_data)
+
+T = 10
+if __name__ == '__main__':
+    times = []
+    for _ in range(T):
+        start = time.time()
+        test_base64_api()
+        # test_file_api()
+        # test_numpy_api()
+        end = time.time()
+        times.append(end - start)
+    ave_time = np.mean(sorted(times)[1:-1])
+    print(f"Ave time: {ave_time}")
--- Code/Tools/FastAPI/image/image_server.py
+++ Code/Tools/FastAPI/image/image_server.py
@@ -5,28 +5,57 @@ import uvicorn
 import json
 import base64
 import numpy as np
+import os, sys
 import cv2
+from io import BytesIO
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+from PIL import Image
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(root_path)
 
 app = FastAPI()
 
 class ImagesIn(BaseModel):
     images: List[str]
 
+class ImagesBytesIn(BaseModel):
+    images: str
 
-@app.post("/images/{image_id}")
-async def image_parse(image_id: int, item: ImagesIn):
+class ImagesNumpyIn(BaseModel):
+    shape: List
+    data: str
+
+@app.post("/images/base64/{image_id}")
+async def image_base64_parse(image_id: int, item: ImagesIn):
     data = json.loads(item.json())
     image_str = data['images'][0]
     image_decode = base64.b64decode(image_str.encode())
     image_np = np.frombuffer(image_decode, np.uint8)
     image = cv2.imdecode(image_np, cv2.COLOR_RGB2BGR)
-    cv2.imshow('test.jpg', image)
-    cv2.waitKey()
-
+    # cv2.imshow('test.jpg', image)
+    # cv2.waitKey()
     return {"image_id": image_id}
 
+async def image_file_parse(image_id: int, file: UploadFile = File(...)):
+    image_tmp_file = file.file
+    image_bytes_stream = BytesIO(image_tmp_file.read())
+    capture_image = Image.open(image_bytes_stream)
+    image = cv2.cvtColor(np.asarray(capture_image), cv2.COLOR_RGB2BGR)
+    # cv2.imshow('test.jpg', image)
+    # cv2.waitKey()
+    return {"image_id": image_id}
+
+@app.post("/images/numpy/{image_id}")
+async def image_numpy_parse(image_id: int, shape: str, file: UploadFile = File(...)):
+    image_tmp_file = file.file
+    image_np = np.frombuffer(image_tmp_file.read(), np.uint8)
+    shape = json.loads(shape)
+    image_np = image_np.reshape(shape)
+    # cv2.imshow('test.jpg', image_np)
+    # cv2.waitKey()
+    return {"image_id": image_id}
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=5000, access_log=True, use_colors=True)
