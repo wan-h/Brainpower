f8aee911d0c16bfc48f740f2e74f14a63f07b8cb
wan-h <13658247573@163.com>
2020-10-13 18:15:44 +0800

    update pytest code

--- Code/Tools/Pytest/test_sample_1.py
+++ Code/Tools/Pytest/test_sample_1.py
@@ -1,12 +1,17 @@
 # coding: utf-8
 # Author: wanhui0729@gmail.com
 
+import pytest
+
 def inc(x):
     return x + 1
 
 def test_answer():
-    assert inc(3) == 5
+    assert inc(3) == 4
 
 """
 pytest test_sample_1.py
 """
+# 或则使用pytest.mian
+if __name__ == '__main__':
+    pytest.main(['-s', 'test_sample_1.py'])
