3477f89750870d078aca447dbe0054b4194d6e6a
wan <13658247573@163.com>
2021-03-16 01:20:08 +0800

    add RabbitMQ/topics

---
+++ Code/Tools/RabbitMQ/topics/receive_logs_topic.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+# Author: wanhui0729@gmail.com
+
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+# python receive_logs_topic.py "kern.*"
+# 类似与泛域名的规则，#代表任意数字，*代表任意字母，以上表示将所有routing_key前缀为kern.的消息路由到该队列
+for binding_key in binding_keys:
+    channel.queue_bind(
+        exchange='topic_logs', queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
