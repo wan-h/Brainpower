8563c1a2b9adf8a7d85fbcbc646835d65439088a
wan <13658247573@163.com>
2021-03-16 01:10:10 +0800

    add RabbitMQ/routing

--- Code/Tools/RabbitMQ/hello/send.py
+++ Code/Tools/RabbitMQ/hello/send.py
@@ -9,7 +9,7 @@ channel = connection.channel()
 # 创建中间件队列
 channel.queue_declare(queue='hello')
 
-# 发布消息，routing_key指定队列名字，消息不直接入队列，通过exchange环节
+# 发布消息，routing_key指定队列名字，消息不直接入队列，通过exchange转发，这里为空，直接转发到名字为routing_key指定的队列
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 # 确保网络缓冲区已刷新并且消息实际上已传递到RabbitMQ
