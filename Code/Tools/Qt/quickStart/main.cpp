531b23249591a3eaa156c9e95cd49d5edc72eb9a
wan <13658247573@163.com>
2021-11-10 23:19:49 +0800

    update Code/Tools/Qt

--- Code/Tools/Qt/quickStart/main.cpp
+++ Code/Tools/Qt/quickStart/main.cpp
@@ -1,23 +1,12 @@
-#include "mainwindow.h"
-
 #include <QApplication>
-
+#include "MainWidget.h"
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication a(argc,argv);
 
-    //添加窗口
-    QWidget widget;
-    //定义一个按钮，它位于 widget 窗口中
-    QPushButton Button("按钮控件", &widget);
-    //设置按钮的位置和尺寸
-    Button.setGeometry(10, 10, 100, 50);
-    //信号与槽，实现当用户点击按钮时，窗口关闭
-    QObject::connect(&Button, &QPushButton::clicked, &widget, &QWidget::close);
-    //让 widget 窗口显示
-    widget.show();
+    MainWidget mWidget;
+    mWidget.show();
 
-    return a.exec();
+    a.exec();
+    return 0;
 }
