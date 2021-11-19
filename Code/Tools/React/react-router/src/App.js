1dce57a66be02ee921b55a43d1e90b0774a691a1
wan-h <13658247573@163.com>
2021-11-19 18:31:55 +0800

    add Code/Tools/React/react-router

---
+++ Code/Tools/React/react-router/src/App.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+// 创建了两个页面 / 和　/about
+// 被嵌套进了html中，使得其他的外围的Welcome to React Router!可以在两个页面中都保留
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
