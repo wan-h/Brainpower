513f0f7dce4e05aeaedff375f63f97cb172e8f0c
Will Wan 万辉 <willw@vastaitech.com>
2023-03-27 10:40:24 +0800

    add streamlit

---
+++ Code/Tools/streamlit/README.md
@@ -0,0 +1,9 @@
+# streamlint
+## 安装
+```
+pip install streamlit
+```
+验证安装成功
+```
+streamlit hello
+```
---
+++ Code/Tools/streamlit/uber_pickups.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# 启动命令：streamlit run uber_pickups.py
+
+# 添加一个title
+st.title('Uber pickups in NYC')
+
+# 页面添加数据展示
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# 缓存数据，参数不改变就不再重复加载数据
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
+# 查看数据
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+# 画直方图
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Plot data on a map
+# st.subheader('Map of all pickups')
+# st.map(data)
+
+# Filter results with a slider
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
