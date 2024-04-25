d5c7f048c47ef25cc720c195e949a049ac88ed6b
wan hui <13658247573@163.com>
2024-04-25 16:52:45 +0800

    update: Code/Tools/llamaindex/quickStart

--- Code/Tools/llamaindex/quickStart.py
+++ Code/Tools/llamaindex/quickStart.py
@@ -1,22 +1,18 @@
-from llama_index.llms.openai import OpenAI
+import os
 from llama_index.core import Settings
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.embeddings import resolve_embed_model
 
-# 配置模型
-print("Loading model...")
-import openai
-openai.api_key = "sk-XXX"
-Settings.llm = OpenAI(temperature=0.2, model="gpt-4")
-# 加载数据
-print("Loading data...")
-documents = SimpleDirectoryReader("data").load_data(show_progress=True)
-# 构建搜索引擎
-print("Building search engine...")
-index = VectorStoreIndex.from_documents(documents, show_progress=True)
-query_engine = index.as_query_engine()
-# 搜索
-print("Searching...")
-response = query_engine.query(
-    "投资的原则是什么？",
+# 使用测试模式
+os.environ["IS_TESTING"] = "True"
+
+# Load data and build an index
+documents = SimpleDirectoryReader('C:\\code\\wanhui\\Brainpower\\Code\\Tools\\LlamaIndex\\data').load_data()
+index = VectorStoreIndex.from_documents(
+    documents,
 )
+
+# Query your data
+query_engine = index.as_query_engine()
+response = query_engine.query("投资理念是什么?")
 print(response)
