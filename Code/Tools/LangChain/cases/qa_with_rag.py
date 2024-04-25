b407485aefe3a107db8b0bb437c7c01657e13f20
wan hui <13658247573@163.com>
2024-04-25 18:40:11 +0800

    update: Code/Tools/LangChain/cases/qa_with_rag

--- Code/Tools/LangChain/cases/qa_with_rag.py
+++ Code/Tools/LangChain/cases/qa_with_rag.py
@@ -1,14 +1,23 @@
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-
+from langchain.llms.fake import FakeListLLM
+from langchain.embeddings.fake import FakeEmbeddings
 
 ############################# fake llm & fake embedding #############################
 
+responses = [
+    "Action: python_repl\nAction Input: print(2 + 2)",
+    "Final Answer: 4"
+]
+llm = FakeListLLM(responses=responses)
+embeddings = FakeEmbeddings(size=512)
 
 
-# 加载数据
+############################# 加载数据 #############################
+print("=" * 100)
+print("loading data...")
 loader = TextLoader('C:\\code\\wanhui\\Brainpower\\Code\\Tools\\LlamaIndex\\data\\财富自由之路1.txt', encoding='utf8')
 docs = loader.load()
-text_splitter = CharacterTextSplitter(separator="\n", is_separator_regex=True, chunk_size=1000, chunk_overlap=200)
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 print(splits)
